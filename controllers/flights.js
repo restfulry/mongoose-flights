2cd9e10e41592731d730208e2e8b4fea530dc4d6
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2020-10-24 16:51:27 -0400

    - add index view - add header -add dropdown menus for add flight

---
+++ controllers/flights.js
@@ -0,0 +1,26 @@
+const Flight = require('../models/flight');
+
+function index(req, res) {
+  Flight.find({}, function(err, flights) {
+    res.render('flights/index', { title: 'All Flights', flights });
+  });
+}
+
+function newFlight(req, res) {
+  res.render('flights/new', { title: 'Add Flight' });
+};
+
+function create(req, res) {
+  const flight = new Flight(req.body);
+  flight.save(function(err) {
+    if(err) return res.redirect('/flights/new');
+    res.redirect('/flights');
+    });
+};
+
+
+module.exports = {
+  index,
+  new: newFlight,
+  create,
+};
---
+++ models/flight.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String, 
+    default: 'DEN',
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date, 
+    default: function() {
+      return new Date(+ new Date() + 365*24*60*60*1000);
+    }
+  },
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
